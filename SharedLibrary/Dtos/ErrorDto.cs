b530bb92905766811d744af95f1c4028a41941b1
Serkan IŞIK <dehnas79@gmail.com>
2023-09-11 16:58:43 +0300

    Add ErrorDto and Response

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,21 @@
+﻿namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<string> Errors { get; private set; } = new List<string>();
+
+        public bool IsShow { get; private set; }
+
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+    }
+}
---
+++ SharedLibrary/Dtos/Response.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SharedLibrary.Dtos
+{
+    public class Response<T> where T : class
+    {
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; }
+
+        [JsonIgnore] // Bu property'i json'a çevirirken göz ardı et. Yani bu property Client'lara açılmayacak...
+        public bool IsSuccessful { get; private set; }  
+        
+        public ErrorDto Error { get; private set; }
+
+        // Geriye data döndürmek istediğimizde kullanacağız.
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        // Geriye data döndürmek istemediğimizde kullanacağız.
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static Response<T> Fail(ErrorDto errorDto, int statusCode)
+        {
+            return new Response<T>
+            {
+                Error = errorDto,
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+        // Tek bir hata olduğunda kullanacağız.
+        public static Response<T> Fail(string errorMessage, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+
+            return new Response<T> { Error = errorDto, StatusCode = statusCode, IsSuccessful = false };
+        }
+    }
+}

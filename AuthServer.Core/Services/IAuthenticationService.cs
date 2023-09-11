a61045fe1a22a58287478294e7156562335b1375
Serkan IŞIK <dehnas79@gmail.com>
2023-09-11 20:16:44 +0300

    Add IAuthenticationService

---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}

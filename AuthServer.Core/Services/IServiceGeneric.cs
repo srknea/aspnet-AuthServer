cf9c2ce3df1d0da55db6f9982599c49df61a4205
Serkan IŞIK <dehnas79@gmail.com>
2023-09-11 17:32:21 +0300

    Add IServiceGeneric and NoDataDto

---
+++ AuthServer.Core/Services/IServiceGeneric.cs
@@ -0,0 +1,20 @@
+﻿using SharedLibrary.Dtos;
+using System.Linq.Expressions;
+
+namespace UdemyAuthServer.Core.Services
+{
+    public interface IServiceGeneric<TEntity, TDto> where TEntity : class where TDto : class
+    {
+        Task<Response<TDto>> GetByIdAsync(int id);
+
+        Task<Response<IEnumerable<TDto>>> GetAllAsync();
+
+        Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
+
+        Task<Response<TDto>> AddAsync(TEntity entity);
+
+        Task<Response<NoDataDto>> Remove(TEntity entity);
+
+        Task<Response<NoDataDto>> Update(TEntity entity);
+    }
+}

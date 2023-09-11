d21ec64e408c2f78e211d88aafaa567fd6300bf6
Serkan IŞIK <dehnas79@gmail.com>
2023-09-11 17:45:32 +0300

    Add TokenDto and ClientTokenDto

---
+++ AuthServer.Core/DTOs/ClientTokenDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthServer.Core.DTOs
+{
+    public class ClientTokenDto
+    {
+        public string AccessToken { get; set; }
+
+        public DateTime AccessTokenExpiration { get; set; }
+    }
+}

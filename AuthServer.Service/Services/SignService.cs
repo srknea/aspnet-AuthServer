b526e02718bfab4c72716a3c31527a8a8cd4190d
Serkan IŞIK <dehnas79@gmail.com>
2023-09-12 15:54:43 +0300

    Add SignService

---
+++ AuthServer.Service/Services/SignService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace AuthServer.Service.Services
+{
+    internal static class SignService
+    {
+        public static SecurityKey GetSymmetricSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}

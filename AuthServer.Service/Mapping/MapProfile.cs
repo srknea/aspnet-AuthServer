b850694c72c7cd5e3cfb792ce21a495ca5c2a477
Serkan IŞIK <dehnas79@gmail.com>
2023-09-12 10:22:58 +0300

    AutoMapper

---
+++ AuthServer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Model;
+using AutoMapper;
+
+namespace AuthServer.Service.Mapping
+{
+    // Sadece AuthServer.Service katmanında kullanılacak
+    internal class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
---
+++ AuthServer.Service/Mapping/ObjectMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using AuthServer.Service.Mapping;
+
+namespace UdemyAuthServer.Service
+{
+    public static class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MapProfile>();
+            });
+
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+}

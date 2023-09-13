6c40760dbdbe4e62f11f4afb3be0dfa248b4087a
Serkan IŞIK <dehnas79@gmail.com>
2023-09-13 10:07:20 +0300

    Add TokenService

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,127 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Model;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SharedLibrary.Configurations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+
+namespace AuthServer.Service.Services
+{
+    internal class TokenService : ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+
+        private readonly CustomTokenOption _tokenOption;
+
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOption> options)
+        {
+            _userManager = userManager;
+            _tokenOption = options.Value;
+        }
+
+        private string CreateRefreshToken()
+        {
+            var numberByte = new Byte[32];
+
+            using var rnd = RandomNumberGenerator.Create();
+
+            rnd.GetBytes(numberByte);
+
+            return Convert.ToBase64String(numberByte);
+        }
+
+        // Üyelik sistemi gerektiren bir token oluşturmak istediğimizde PAYLOAD'ı dolduracak
+        private IEnumerable<Claim> GetClaims(UserApp userApp, List<string> audiences)
+        {
+            var userList = new List<Claim> {
+            new Claim(ClaimTypes.NameIdentifier,userApp.Id),
+            new Claim(JwtRegisteredClaimNames.Email, userApp.Email),
+            new Claim(ClaimTypes.Name,userApp.UserName),
+            new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            userList.AddRange(audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+
+            return userList;
+        }
+
+        // Üyelik sistemi gerektirmeyen bir token oluşturmek istediğimizde PAYLOAD'ı dolduracak
+        private IEnumerable<Claim> GetClaimsByClient(Client client)
+        {
+            var claims = new List<Claim>();
+            claims.AddRange(client.Audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString());
+            new Claim(JwtRegisteredClaimNames.Sub, client.Id.ToString()); // Sub: Subject ---> Bu token kime ait
+
+            return claims;
+        }
+
+        // Token oluşturmak için kullanacağımız metot
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            var accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOption.AccessTokenExpiration); // Token'ın ömrü
+            var refreshTokenExpiration = DateTime.Now.AddMinutes(_tokenOption.RefreshTokenExpiration);
+            var securityKey = SignService.GetSymmetricSecurityKey(_tokenOption.SecurityKey); // Token'ı imzalayacak olan key
+
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature); // Token'ı imzalayacak olan algoritma. HEADER kısmında şu şekilde belirtilecek ---> alg:HS256
+
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(
+                issuer: _tokenOption.Issuer,
+                expires: accessTokenExpiration,
+                 notBefore: DateTime.Now,
+                 claims: GetClaims(userApp, _tokenOption.Audience),
+                 signingCredentials: signingCredentials);
+
+            var handler = new JwtSecurityTokenHandler(); // Token'ı oluşturacak olan nesne
+
+            var token = handler.WriteToken(jwtSecurityToken); // Token'ı oluşturuyoruz
+
+            var tokenDto = new TokenDto
+            {
+                AccessToken = token,
+                RefreshToken = CreateRefreshToken(),
+                AccessTokenExpiration = accessTokenExpiration,
+                RefreshTokenExpiration = refreshTokenExpiration
+            };
+
+            return tokenDto;
+        }
+
+        // Üyelik sistemi gerektirmeyen Client'lar için token oluşturacağımız method
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            var accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOption.AccessTokenExpiration);
+
+            var securityKey = SignService.GetSymmetricSecurityKey(_tokenOption.SecurityKey);
+
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(
+                issuer: _tokenOption.Issuer,
+                expires: accessTokenExpiration,
+                 notBefore: DateTime.Now,
+                 claims: GetClaimsByClient(client),
+                 signingCredentials: signingCredentials);
+
+            var handler = new JwtSecurityTokenHandler();
+
+            var token = handler.WriteToken(jwtSecurityToken);
+
+            var tokenDto = new ClientTokenDto
+            {
+                AccessToken = token,
+
+                AccessTokenExpiration = accessTokenExpiration,
+            };
+
+            return tokenDto;
+        }
+    }
+}

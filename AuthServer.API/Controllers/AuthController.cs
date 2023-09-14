f3091f0692a6622c929e993d119b31f02382a7c2
Serkan IŞIK <dehnas79@gmail.com>
2023-09-14 08:46:48 +0300

    Add AuthController and CustomBaseController

---
+++ AuthServer.API/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using UdemyAuthServer.API.Controllers;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : CustomBaseController
+    {
+        private readonly ICustomAuthenticationService _customAuthenticationService;
+
+        public AuthController(ICustomAuthenticationService customAuthenticationService)
+        {
+            _customAuthenticationService = customAuthenticationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(LoginDto loginDto)
+        {
+            var result = await _customAuthenticationService.CreateTokenAsync(loginDto);
+
+            /*
+            if (result.StatusCode == 200)
+            {
+                return Ok();
+            }
+            else if(result.StatusCode == 404)
+            {
+                return NotFound();
+            }
+            */
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            var result = _customAuthenticationService.CreateTokenByClient(clientLoginDto);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RevokeRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _customAuthenticationService.RevokeRefreshToken(refreshTokenDto.RefreshToken);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshToken(RefreshTokenDto refreshTokenDto)
+
+        {
+            var result = await _customAuthenticationService.CreateTokenByRefreshToken(refreshTokenDto.RefreshToken);
+
+            return ActionResultInstance(result);
+        }
+    }
+}
---
+++ AuthServer.Core/DTOs/RefreshTokenDto.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthServer.Core.DTOs
+{
+    public class RefreshTokenDto
+    {
+        public string RefreshToken { get; set; }
+    }
+}

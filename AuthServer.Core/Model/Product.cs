3f1881c8aa6c03b6c4e865fbd83a626ef1c92160
Serkan IŞIK <dehnas79@gmail.com>
2023-09-14 17:16:07 +0300

    Add ProductController

--- AuthServer.Core/Model/Product.cs
+++ AuthServer.Core/Model/Product.cs
@@ -13,6 +13,7 @@ namespace AuthServer.Core.Model
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
-        public int UserId { get; set; } 
+
+        public string UserId { get; set; } 
     }
 }

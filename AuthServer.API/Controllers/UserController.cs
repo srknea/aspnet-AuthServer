89857736c33e0e44a505bf685d7fa6aca1a737e0
Serkan IŞIK <dehnas79@gmail.com>
2023-09-14 08:54:48 +0300

    Add UserController and update Program.cs

---
+++ AuthServer.API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UdemyAuthServer.API.Controllers;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : CustomBaseController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        //api/user
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserDto createUserDto)
+        {
+            return ActionResultInstance(await _userService.CreateUserAsync(createUserDto));
+        }
+
+        [Authorize] // Bu endpoint e erişmek için token gerekli
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            return ActionResultInstance(await _userService.GetUserByNameAsync(HttpContext.User.Identity.Name));
+        }
+    }
+}
--- AuthServer.API/Program.cs
+++ AuthServer.API/Program.cs
@@ -82,6 +82,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();

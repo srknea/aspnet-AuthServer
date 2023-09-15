c6f5a83ffee1d5146eb6328ead587212d1455809
Serkan IŞIK <dehnas79@gmail.com>
2023-09-15 13:43:27 +0300

    Update for mini apps

--- SecondMiniApp.API/Program.cs
+++ SecondMiniApp.API/Program.cs
@@ -1,3 +1,6 @@
+using SharedLibrary.Configurations;
+using SharedLibrary.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
+var tokenOptions = builder.Configuration.GetSection("TokenOption").Get<CustomTokenOption>();
+
+builder.Services.AddCustomTokenAuth(tokenOptions);
 
 var app = builder.Build();
 
@@ -17,6 +25,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
